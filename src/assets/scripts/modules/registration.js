17aab6f366031727a7049b1915db5979e9fe4791
Alexander Varnaev <alexandervarnaev@gmail.com>
2019-01-31 13:38:08 +0300

    popup center, background coins

--- src/assets/scripts/modules/registration.js
+++ src/assets/scripts/modules/registration.js
@@ -1,24 +1,29 @@
 var btn = document.querySelector(".button"),
     closeReg = document.querySelector(".registration__close"),
     regForm = document.querySelector(".registration"),
-    warning = document.querySelector('.warning__text'),
-    content = document.querySelector('.content');
-    
+    iFrame = document.querySelector(".registration__frame");
+
+var changePosition = function() {
+    var framePosition = iFrame.getBoundingClientRect(),
+        top = framePosition.top,
+        right = framePosition.left;
+
+    closeReg.style.top = top + "px";
+    closeReg.style.right = right + "px";
+};
+
 btn.addEventListener("click", function(e) {
     e.preventDefault;
     regForm.style.display = "block";
-    btn.style.display = "none";
-    warning.style.color = "#fff";
-    if (regForm.style.position = "absolute") {
-        content.firstElementChild.style.position ="absolute";
-        content.firstElementChild.style.top ="5%";
-    }
+
+    changePosition();
 });
 
 regForm.addEventListener("click", function(e) {
     e.preventDefault;
     regForm.style.display = "none";
-    btn.style.display = "block";
-    warning.style.color = "transparent";
-    content.firstElementChild.style.position ="initial";
 });
+
+window.onresize = function() {
+    changePosition();
+};
